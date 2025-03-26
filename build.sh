ada82ab6b59a762a11a3771f3d2b8c6cda253b32
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-26 15:03:41 +0545

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,25 @@
+sudo apt install tar xz-utils arch-install-scripts
+wget https://repo-default.voidlinux.org/static/xbps-static-latest.x86_64-musl.tar.xz
+wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage
+chmod +x appimagetool-x86_64.AppImage
+tar xf xbps-static-latest.x86_64-musl.tar.xz
+export XBPS_ARCH=x86_64  
+export ARCH=x86_64
+APP="mpv"
+mkdir app
+ ./usr/bin/xbps-install -Sy -r $APP.AppDir -R "https://repo-default.voidlinux.org/current" $APP
+
+ cat >> ./$APP/$APP.AppDir/AppRun << 'EOF'
+#!/bin/sh
+HERE="$(dirname "$(readlink -f "${0}")")"
+export PATH="${HERE}"/usr/bin/:"${HERE}"/usr/sbin/:"${HERE}"/usr/games/:"${PATH}"
+export LD_LIBRARY_PATH=/lib/:/lib64/:/usr/lib/:/usr/lib/x86_64-linux-gnu/:"${HERE}"/usr/lib/:"${HERE}"/usr/lib/x86_64-linux-gnu/:"${HERE}"/lib/:"${HERE}"/lib/x86_64-linux-gnu/:"${LD_LIBRARY_PATH}"
+export PYTHONPATH="${HERE}"/usr/share/python3/:"${PYTHONPATH}"
+export XDG_DATA_DIRS="${HERE}"/usr/share/:"${XDG_DATA_DIRS}"
+export MPV_CONFIG_PATH=$HERE/etc/mpv/:$MPV_CONFIG_PATH
+exec ${HERE}/usr/bin/mpv "$@"
+EOF
+chmod a+x ./$APP/$APP.AppDir/AppRun
+mv ./AppRun ./$APP/$APP.AppDir
+ 
+ ARCH=x86_64 ./appimagetool-x86_64.AppImage $APP.AppDir
